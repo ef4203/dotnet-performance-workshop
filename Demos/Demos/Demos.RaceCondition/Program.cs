c4d0165d22a9222a0c4543dfa0222a32760a4c24
Elias Frank <32647755+ef4203@users.noreply.github.com>
2024-04-28 15:36:58 +0200

    Added race condition demo

---
+++ Demos/Demos/Demos.RaceCondition/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Demos.RaceCondition;
+
+public class Program
+{
+    private static int counter = 0;
+
+    public static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(IncrementCounter);
+        Thread thread2 = new Thread(IncrementCounter);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("Final counter value: " + counter);
+    }
+
+    static void IncrementCounter()
+    {
+        for (int i = 0; i < 1000000; i++)
+        {
+            counter++;
+        }
+    }
+}
